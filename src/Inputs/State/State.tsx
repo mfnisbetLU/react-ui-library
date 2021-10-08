0b97ab18380e70c303d233b6a381e2169b313943
Supanat Wangsutthitham <soup.wangsutthitham@mail.utoronto.ca>
2021-10-08 00:28:57 -0400

    other: testing permission

--- src/Inputs/State/State.tsx
+++ src/Inputs/State/State.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { useTheme  } from 'styled-components';
+// import { useTheme } from 'styled-components';
 
-import { LabelLayout, LabelLayoutProps  } from '@Layouts';
+import { LabelLayout, LabelLayoutProps } from '@Layouts';
 
 // values that we want to be able to change
 export interface StateProps extends LabelLayoutProps {
@@ -19,11 +19,12 @@ export const State: React.FC<StateProps> = ({
     // size: string;
     ...props
 }: StateProps): React.ReactElement => {
-    const theme = useTheme();
+    // const theme = useTheme();
 
     return (
         <LabelLayout>
             <div>Hello World!</div>
+            <div>UwU!</div>
         </LabelLayout>
     );
 };
