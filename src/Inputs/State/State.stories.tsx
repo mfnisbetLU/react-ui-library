62e8d59b12bda5798deb491d14b34c5596efc4cd
Khatchig Anteblian <ainteblian99@gmail.com>
2021-10-10 00:48:57 -0400

    added icon color transitions

--- src/Inputs/State/State.stories.tsx
+++ src/Inputs/State/State.stories.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import { Meta, Story } from '@storybook/react';
 import { State, StateProps } from '../../index';
 import { createStoryTitle } from '../../Constants';
+import { ShoppingBag, Payment, Food } from '@styled-icons/fluentui-system-filled';
 
 export default {
     title: createStoryTitle('State'),
     component: State,
     args: {
-        label: 'state',
-        description: 'description',
+        states: [ShoppingBag, Food, Payment],
+        colors: ['#ddd', '#f00'],
+        currentState: 0,
+        size: '3em',
     },
 } as Meta;
 
--- src/Inputs/State/State.tsx
+++ src/Inputs/State/State.tsx
@@ -1,39 +1,64 @@
 import React from 'react';
 import { LabelLayout, LabelLayoutProps  } from '@Layouts';
 import styled from 'styled-components';
-import { ShoppingBag, Payment, Food } from '@styled-icons/fluentui-system-filled';
 import { StyledIcon } from 'styled-icons/types';
 
-// values that we want to be able to change
 export interface StateProps extends LabelLayoutProps {
     states: StyledIcon[];
+    colors: string[];
     currentState: number;
+    size: string;
     // onChange: function;
     // iconProps: { colour: string };
     // size: string;
 }
 
-// actual component
 export const State: React.FC<StateProps> = ({
     // state: int;
     // onChange: function;
     // iconProps: { colour: string };
     // size: string;
-    states = [Food, ShoppingBag, Payment],
-    currentState = 0,
+    states,
+    colors,
+    currentState,
+    size,
     ...props
 }: StateProps): React.ReactElement => {
     //const theme = useTheme();
+    let elements = [];
 
-    const StateIcon = styled(states[currentState])`
-        height: 3em;
-        width: 3em;
-        color: #ddd;
+    const ColDiv = styled.div`
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        margin: 3%;
+    `
+
+    for (let i=0; i<states.length; i++) {
+        let currentElement =
+            <ColDiv>
+                {React.createElement(
+                    states[i],
+                    {
+                        height: size,
+                        width: size,
+                        color: (currentState >= i) ? colors[1] : colors[0],
+                    }
+                )}
+                Hello
+            </ColDiv>;
+
+        elements.push(currentElement);
+    }
+
+    const RowDiv = styled.div`
+        display: flex;
+        flex-direction: row;
     `
 
     return (
-        //<div>{handleChangeState(currentState)}</div>
-        <StateIcon />
+        <RowDiv>
+            {elements}
+        </RowDiv>
     );
 };
-
