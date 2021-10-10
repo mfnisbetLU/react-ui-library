28099130cd9ec52469ee1d3c612c9daf92e7b714
Khatchig Anteblian <ainteblian99@gmail.com>
2021-10-09 23:32:23 -0400

    implemented basic state transition functionality

--- src/Inputs/State/State.tsx
+++ src/Inputs/State/State.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import { useTheme  } from 'styled-components';
 
 import { LabelLayout, LabelLayoutProps  } from '@Layouts';
+import styled from 'styled-components';
+import { ShoppingBag, Payment, Food } from '@styled-icons/fluentui-system-filled';
+import { StyledIcon } from 'styled-icons/types';
 
 // values that we want to be able to change
 export interface StateProps extends LabelLayoutProps {
-    // state: int;
+    states: StyledIcon[];
+    currentState: number;
     // onChange: function;
     // iconProps: { colour: string };
     // size: string;
@@ -17,13 +20,21 @@ export const State: React.FC<StateProps> = ({
     // onChange: function;
     // iconProps: { colour: string };
     // size: string;
+    states = [Food, ShoppingBag, Payment],
+    currentState = 0,
     ...props
 }: StateProps): React.ReactElement => {
-    const theme = useTheme();
+    //const theme = useTheme();
+
+    const StateIcon = styled(states[currentState])`
+        height: 3em;
+        width: 3em;
+        color: #ddd;
+    `
 
     return (
-        <LabelLayout>
-            <div>Hello World!</div>
-        </LabelLayout>
+        //<div>{handleChangeState(currentState)}</div>
+        <StateIcon />
     );
 };
+
